b45baf08120ac5e99186806d5f235a5a0fecddda
jane <3630911527@qq.com>
2024-07-25 16:50:43 +0800

    购买飞机票

--- src/com/itheima/Test1.java
+++ src/com/itheima/Test1.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 //计算飞机票价格
 public class Test1 {
     public static void main(String[] args) {
-        int price;
+        int price = 0;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("请输入机票的原价：");
@@ -22,32 +22,15 @@ public class Test1 {
             int i = sc.nextInt();
             //如果是旺季（5-10月）头等舱9折   经济舱8.5折
             if (month >= 5 && month <= 10) {
-                getPrice(ticket, i, 0.9,0.85);
-//                if (i == 1) {
-//                    price = (int) (ticket * 0.9);
-//                    System.out.println("头等舱现价为：" + price);
-//                } else if (i == 2) {
-//                    price = (int) (ticket * 0.85);
-//                    System.out.println("经济舱现价为：" + price);
-//                }else {
-//                    System.out.println("没有这个舱位");
-//                }
+//                getPrice(ticket, i, 0.9,0.85);
+                extracted(i, price, ticket, 0.9, 0.85);
 
             }
             //如果是淡季（11月-来年4月）头等舱7折   经济舱6.5折
             else {
                 if ((month >= 1 && month <= 4) || (month >= 11 && month <= 12)) {
-                    getPrice(ticket, i, 0.7,0.65);
-//                    if (i == 1) {
-//                        price = (int) (ticket * 0.7);
-//                        System.out.println("头等舱现价为：" + price);
-//                    } else if (i == 2) {
-//                        price = (int) (ticket * 0.65);
-//                        System.out.println("经济舱现价为：" + price);
-//                    }
-//                    else {
-//                        System.out.println("没有这个舱位");
-//                    }
+//                    getPrice(ticket, i, 0.7,0.65);
+                    extracted(i, price, ticket, 0.7, 0.65);
                 }
             }
         } else {
@@ -55,8 +38,20 @@ public class Test1 {
         }
     }
 
+    private static void extracted(int i, int price, int ticket, double x, double x1) {
+        if (i == 1) {
+            price = (int) (ticket * x);
+            System.out.println("头等舱现价为：" + price);
+        } else if (i == 2) {
+            price = (int) (ticket * x1);
+            System.out.println("经济舱现价为：" + price);
+        } else {
+            System.out.println("没有这个舱位");
+        }
+    }
 
-    //将重复部分抽取成方法
+
+   /* //将重复部分抽取成方法
     public static int getPrice(int ticket, int i, double v1, double v2) {
         int price = 0;
         if (i == 1) {
@@ -71,7 +66,7 @@ public class Test1 {
         }
 
         return price;
-    }
+    }*/
 }
 
 
